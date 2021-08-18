a7c228dd3261ccbd58bed3c684d305b8eeff5eff
ilyasidor <63228249+ilyasidor@users.noreply.github.com>
2021-08-18 19:48:37 +0900

    Update Program.cs

--- destructive_magic_programm/destructive_magic_programm/Program.cs
+++ destructive_magic_programm/destructive_magic_programm/Program.cs
@@ -1,31 +1,27 @@
-﻿
-using Microsoft.Win32;
-using System;
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Media;
 using System.Runtime.InteropServices;
 using System.Threading;
-using IronPython.Hosting;
-using Microsoft.Scripting.Hosting;
-using NAudio.Wave;
 
 namespace destructive_magic_programm
 {
     class Program
     {
+        delegate void Action();
         static void Main(string[] args)
         {
-            StartSoundWithImages();     
+            Action magic = StartSound;
+            magic += ChangeWallpaper;
+            magic();
         }
-
-        public static void StartSoundWithImages()
+        public static void ChangeWallpaper()
         {
             wallpapers wallpapers = new wallpapers();
-            StartSound();
             int count = 0;
-            while (count<=2000)
+            while (count <= 2000)
             {
                 foreach (string file in wallpapers)
                 {
@@ -37,16 +33,79 @@ namespace destructive_magic_programm
         }
         public static void StartSound()
         {
-            SoundPlayer sp;
-            sp = new SoundPlayer(@"C:\Users\Ilya\source\repos\destructive_magic_programm\destructive_magic_programm\audios\SUPER_SUS_-_Pubertat_64680041.wav");
-            sp.Play();
+            Audios audios = new Audios();
+            try
+            {
+                string path_to_audio = audios.path_files[0];
+                SoundPlayer sp;
+                sp = new SoundPlayer(@$"{path_to_audio}");
+                sp.Play();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("В папке 'audios' нету файлов вообще или нету файлов wav");
+            }
         }
         
     }
+    class Audios
+    {
+        static string path_audios = GetPathAudio();
+        static string GetPathAudio()
+        {
+            string[] DirsInCurrentDirectory = Directory.GetDirectories(
+                AppDomain.CurrentDomain.BaseDirectory);
+            string path_audios = null;
+
+            foreach (string item in DirsInCurrentDirectory)
+            {
+                if (item.EndsWith("audios"))
+                {
+                    path_audios = item;
+                }
+            }
+            return path_audios;
+        }
+
+        string[] files = Directory.GetFiles(path_audios);
+        public int Lenght
+        {
+            get
+            {
+                return path_files.Count;
+            }
+        }
+
+        public List<string> path_files = new List<string>();
+        public Audios()
+        {
+            Add_Files_in_list();
+        }
+        void Add_Files_in_list()
+        {
+            foreach (string file_name in files)
+            {
+                if (file_name.EndsWith(".wav"))
+                {
+                    path_files.Add(file_name);
+                }
+                else
+                {
+                    System.IO.File.Delete($@"{file_name}");
+                }
+            }
+        }
+        public IEnumerator GetEnumerator()
+        {
+            return path_files.GetEnumerator();
+        }
+    }
     class wallpapers
     {
-        string[] files = Directory.GetFiles("C:\\Users\\Ilya\\source\\repos\\destructive_magic_programm\\destructive_magic_programm\\photos");
+        public static string path_photo = GetPathPhotos();
+        string[] files = Directory.GetFiles(path_photo);
         public List<string> path_files = new List<string>(); 
+
         public wallpapers()
         {
             Add_Files_in_list();
@@ -58,14 +117,29 @@ namespace destructive_magic_programm
                 return path_files.Count;
             }
         }
+
+        
+        public static string GetPathPhotos()
+        {
+            string[] dirs = Directory.GetDirectories(AppDomain.CurrentDomain.BaseDirectory);
+            string path_photos = null;
+            foreach (string item in dirs)
+            {
+                if (item.EndsWith("photos"))
+                {
+                    path_photos = item;
+                }
+            }
+            return path_photos;
+        }
         public void Add_Files_in_list()
         {
-            
             foreach (string file_name in files)
             {
                 path_files.Add(file_name);
             }
         }
+
         public IEnumerator GetEnumerator()
         {
             return path_files.GetEnumerator();
